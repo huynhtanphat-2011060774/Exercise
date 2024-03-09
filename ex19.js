b88470d56369f3e932dfd9a4e957c90f4498af6d
huynhtanphat-2011060774 <didvp06@gmail.com>
2024-03-09 16:14:49 +0700

    Exercise

--- ex19.js
+++ ex19.js
@@ -2,7 +2,6 @@ function checkIntegerWithinRange(number) {
     return (Math.abs(100 - number) <= 20) || (Math.abs(400 - number) <= 20);
   }
   
-  // Example usage
   const givenInteger = 90;
   const result = checkIntegerWithinRange(givenInteger);
   console.log(`Result: ${result}`);
